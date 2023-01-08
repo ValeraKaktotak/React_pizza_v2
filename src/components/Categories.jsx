a5f72d6e3cf42ec49895a3c7f44120a33b900bcc
Valera <valerakaktotak@gmail.com>
2023-01-08 04:09:00 +0200

    add active class

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,61 @@
+import { useState } from 'react'
+
 const Categories = () => {
+  let [active, setActive] = useState(0)
+  const addActive = (index) => {
+    setActive(index)
+  }
   return (
-    <div class="categories">
+    <div className="categories">
       <ul>
-        <li class="active">Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        <li
+          onClick={() => {
+            addActive(0)
+          }}
+          className={active === 0 ? 'active' : ''}
+        >
+          Все
+        </li>
+        <li
+          onClick={() => {
+            addActive(1)
+          }}
+          className={active === 1 ? 'active' : ''}
+        >
+          Мясные
+        </li>
+        <li
+          onClick={() => {
+            addActive(2)
+          }}
+          className={active === 2 ? 'active' : ''}
+        >
+          Вегетарианская
+        </li>
+        <li
+          onClick={() => {
+            addActive(3)
+          }}
+          className={active === 3 ? 'active' : ''}
+        >
+          Гриль
+        </li>
+        <li
+          onClick={() => {
+            addActive(4)
+          }}
+          className={active === 4 ? 'active' : ''}
+        >
+          Острые
+        </li>
+        <li
+          onClick={() => {
+            addActive(5)
+          }}
+          className={active === 5 ? 'active' : ''}
+        >
+          Закрытые
+        </li>
       </ul>
     </div>
   )
