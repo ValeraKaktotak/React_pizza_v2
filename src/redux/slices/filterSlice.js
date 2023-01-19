0d0df1c2045840f002533eb3a70e27cbb9abdafa
Valera <valerakaktotak@gmail.com>
2023-01-19 15:38:19 +0200

    create empty store and example slice

---
+++ src/redux/slices/filterSlice.js
@@ -0,0 +1,25 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const initialState = {
+  count: 0,
+}
+
+export const filterSlice = createSlice({
+  name: 'counter',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.count += 1
+    },
+    decrement: (state) => {
+      state.count -= 1
+    },
+    incrementByAmount: (state, action) => {
+      state.count += action.payload
+    },
+  },
+})
+
+export const { increment, decrement, incrementByAmount } = filterSlice.actions
+
+export default filterSlice.reducer
