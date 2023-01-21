e6c588444dfe13278bf8d3120bd457e74eb717ff
Valera <valerakaktotak@gmail.com>
2023-01-21 18:05:07 +0200

    fixed paginator

--- src/components/Paginator/index.jsx
+++ src/components/Paginator/index.jsx
@@ -1,9 +1,10 @@
 import ReactPaginate from 'react-paginate'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { setPaginatorPage } from 'redux/slices/filterSlice'
 import style from './Paginator.module.scss'
 
 function Paginator({ pageCount = 3 }) {
+  const startPage = useSelector((state) => state.filterReducer.paginatorPage)
   const dispatch = useDispatch()
   return (
     <ReactPaginate
@@ -15,6 +16,7 @@ function Paginator({ pageCount = 3 }) {
         dispatch(setPaginatorPage(e.selected + 1))
       }}
       pageRangeDisplayed={4}
+      forcePage={startPage - 1}
       pageCount={pageCount}
       renderOnZeroPageCount={null}
     />
