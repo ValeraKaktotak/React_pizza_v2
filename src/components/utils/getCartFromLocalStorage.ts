c70428ff5ac5dc00952727dccccf1c1c054c0a3d
ValeraKaktotak <valerakaktotak@gmail.com>
2023-07-02 14:09:35 +0300

    small refactoring

--- src/components/utils/getCartFromLocalStorage.ts
+++ src/components/utils/getCartFromLocalStorage.ts
@@ -3,15 +3,7 @@ export const getCartFromLocalStorage = () => {
     const data = localStorage.getItem('pizzaCart')
     if(data){
         const json = JSON.parse(data)
-        return json.items
+        return json
     }
     return []
 } 
-export const  getTotalCostFromLocalStorage = () => {
-    const data = localStorage.getItem('pizzaCart')
-    if(data){
-        const json = JSON.parse(data)
-        return json.totalCoast
-    }
-    return 0
-}
